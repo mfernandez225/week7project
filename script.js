3029eb8a0c1583c41dabc0912757f13007924920
Marc Fernandez <mfernandez225@gmail.com>
2020-01-02 21:07:45 -0800

    APi's for jokes, qiphys, quotes

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+  // This is the Jokes API Call
   var queryURL = "https://sv443.net/jokeapi/category/Programming";
 
   $.ajax({
     url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+  // This is the Giphy API Call
+  var queryURL2 = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+  $.ajax({
+    url: queryURL2,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  });
+  // This is the Quotes API Call
+  var queryURL3 = "";
+
+  $.ajax({
+    url: queryURL3,
     method: "GET"
   }).then(function (response) {
     console.log(response);
