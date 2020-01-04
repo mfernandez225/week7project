a8c03c2d67e672210fd7af9c7a6714df10a28cd7
Marc Fernandez <mfernandez225@gmail.com>
2020-01-04 10:19:12 -0800

    quote API call

--- script.js
+++ script.js
@@ -16,12 +16,20 @@
   }).then(function (response) {
     console.log(response);
   });
-  // This is the Quotes API Call
-  var queryURL3 = "";
 
-  $.ajax({
-    url: queryURL3,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-  });
+  // This is the Quotes API Call (This is functioning I just disabled to not pull quotes all the time.gi)
+
+  // var getQuote = {
+  //   "async": true,
+  //   "crossDomain": true,
+  //   "url": "https://quotable-quotes.p.rapidapi.com/randomQuotes",
+  //   "method": "GET",
+  //   "headers": {
+  //     "x-rapidapi-host": "quotable-quotes.p.rapidapi.com",
+  //     "x-rapidapi-key": "8dba9d69a1msh6c225bacd1ec6ecp1d626cjsn9856a04e02e3"
+  //   }
+  // }
+
+  // $.ajax(getQuote).done(function (response) {
+  //   console.log(response);
+  // });
