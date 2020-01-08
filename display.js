0b5f70253e7696a130db3bc4074f0922413dca91
Marc Fernandez <mfernandez225@gmail.com>
2020-01-07 21:35:50 -0800

    display tweaks

--- display.js
+++ display.js
@@ -1,9 +1,11 @@
 // $(".backdrop").attr('style', `background-color: ${localStorage.getItem("answer_1")}` )
 
 $(document).ready(function () {
-    $(".backdrop").attr('style', `background-color: ${localStorage.getItem("answer_1")}`)
+    $("#backdrop").attr('style', `background-color: ${localStorage.getItem("answer_1")}`)
     $('#nameDrop').append(localStorage.getItem("user_name"))
 
 })
 
-//button click to play audio based off genre. most popular
+$("#yes").on("click", function () {
+
+})
