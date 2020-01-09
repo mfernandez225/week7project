db7ae243b4a2b60478b290abc9f201cafb4911ba
Kayla San Filippo <Kaylasf@Kaylas-MacBook-Air.local>
2020-01-09 13:23:56 -0800

    loaders, bootstrap, media query

--- display.js
+++ display.js
@@ -1,3 +1,11 @@
+$(function(){
+    $(".loader").fadeOut(1000,function(){
+      $(".content").fadeIn(500)
+      $(".loadingContainer").attr('style',"display:none")
+      $('body').attr('style', "background-color: #404040")
+    })
+  })
+
 $(document).ready(function () {
     $(".colorSelected").attr('style', `background-color: ${localStorage.getItem("answer_1")}`)
     $('.nameDrop').append(localStorage.getItem("user_name"))
